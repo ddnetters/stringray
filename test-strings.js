35916d4b2e5ef1c8531fcbb914852303548907fa
ddnetters <d.d.netters@gmail.com>
2026-01-28 13:52:01 +0100

    Add end-to-end test workflow for StringRay action
    - Add test-stringray.yml workflow to validate action works in CI
    - Add test-strings.js with intentional errors for testing
    - Fix $VERSION variable escaping in release.yml markdown

---
+++ test-strings.js
@@ -0,0 +1,4 @@
+// Test file with intentional errors
+const message1 = "This is a test messaeg";  // spelling error
+const message2 = "recieve the data";        // spelling error
+const message3 = "Hello world";             // valid
